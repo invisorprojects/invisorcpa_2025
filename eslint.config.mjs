97c9dd44925258002e8bc9efb866240debba8929
govindpvenu.txt@gmail.com <govindpvenu.txt@gmail.com>
2025-06-05 19:38:13 +0530

    chore: setup prettier and eslint; update outdated packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,36 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import unusedImports from 'eslint-plugin-unused-imports';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.extends('next/core-web-vitals', 'next/typescript'),
+    {
+        // Define plugins as an object, not an array
+        plugins: {
+            'unused-imports': unusedImports,
+        },
+        rules: {
+            '@typescript-eslint/no-unused-vars': 'off', // Turn off the TypeScript rule
+            'unused-imports/no-unused-imports': 'warn', // Error on unused imports
+            'unused-imports/no-unused-vars': [
+                'warn',
+                {
+                    vars: 'all',
+                    varsIgnorePattern: '^_',
+                    args: 'after-used',
+                    argsIgnorePattern: '^_',
+                },
+            ],
+            '@typescript-eslint/no-explicit-any': 'warn',
+        },
+    },
 ];
 
 export default eslintConfig;
