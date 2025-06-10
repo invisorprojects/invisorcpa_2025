91db5af15147d05058efa1485484c908f1bfe776
govindpvenu.txt@gmail.com <govindpvenu.txt@gmail.com>
2025-06-10 15:37:47 +0530

    refactor: ran prettier write .

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,9 +1,9 @@
-import { Metadata } from "next";
+import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "Services",
+    title: 'Services',
 };
 
 export default function Page() {
-  return <div>Services</div>;
+    return <div>Services</div>;
 }
