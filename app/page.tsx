4706a5cd408f6dce4af2a542cdb9846852d3e985
govindpvenu.txt@gmail.com <govindpvenu.txt@gmail.com>
2025-06-11 15:03:20 +0530

    feat: section 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image';
 import TrustedPartners from '@/components/trusted-partners';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
     return (
-        <main className="px-2 sm:px-4 md:px-8 lg:px-12 xl:px-24">
-            <section className="flex flex-col justify-between gap-4">
+        <main className="">
+            <section className="flex flex-col justify-between gap-4 px-2 sm:px-4 md:px-8 lg:px-12 xl:px-24">
                 <div className="mt-4">
                     <h1 className="text-4xl font-bold md:text-5xl 2xl:text-6xl">
                         Trust Us to Handle What Counts with Financial Support
@@ -30,6 +31,58 @@ export default function Home() {
                     </div>
                 </div>
                 <TrustedPartners />
+            </section>
+            <section className="flex flex-col bg-[#EFF0F4] px-2 sm:flex-row sm:px-4 md:px-8 lg:px-12 xl:px-24">
+                <div className="p-4 sm:w-1/2">
+                    <Image
+                        className="h-auto w-full rounded-3xl"
+                        src="/assets/section-2-1.webp"
+                        alt="Hero"
+                        width={1199}
+                        height={899}
+                    />
+                </div>
+                <div className="flex flex-col items-center justify-between p-4 sm:w-1/2">
+                    <div className="flex flex-col gap-4">
+                        <h3 className="text-4xl font-bold">
+                            Simplify Your Accounts, Maximize Your Success!
+                        </h3>
+                        <p>
+                            Whether you’re managing accounting or navigating
+                            taxes, our dedicated team is here to support you. We
+                            provide expert accounting, tax planning, and
+                            bookkeeping services to simplify your accounting
+                            journey with efficient, tech-driven solutions. We
+                            take the stress out of financial management so you
+                            can focus on what matters most—growing your
+                            business. With personalized support and smart
+                            automation, we ensure accuracy, compliance, and
+                            peace of mind at every step.
+                        </p>
+                    </div>
+
+                    <div className="flex flex-col items-center justify-center gap-4 sm:flex-row">
+                        <Image
+                            className="rounded-3xl"
+                            src="/assets/section-2-2.webp"
+                            alt="Hero"
+                            width={300}
+                            height={300}
+                        />
+                        <div className="flex flex-col items-start justify-between gap-4 py-4">
+                            <h4 className="text-xl font-bold">
+                                Canada’s Leading Bookkeeping Service providers –
+                                Trusted by Over 1,000 Clients Nationwide
+                            </h4>
+                            <p>
+                                At Invisor, we handle your bookkeeping with
+                                precision, so you can focus on growing your
+                                business.
+                            </p>
+                            <Button className="rounded-full">Learn More</Button>
+                        </div>
+                    </div>
+                </div>
             </section>
         </main>
     );
