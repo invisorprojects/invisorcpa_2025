b01e61df2969affe5a5c3f0597a620c4e1928b8a
govindpvenu.txt@gmail.com <govindpvenu.txt@gmail.com>
2025-06-13 15:21:44 +0530

    feat: services page grid

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,3 +1,5 @@
+import ServicesCard from '@/components/ServicesCard';
+import services from '@/constants/services';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -5,5 +7,20 @@ export const metadata: Metadata = {
 };
 
 export default function Page() {
-    return <main>Services</main>;
+    return (
+        <main>
+            <section className="flex flex-col items-center justify-between p-4 sm:p-8 md:p-12 lg:p-16 xl:p-24"></section>
+            <section className="flex flex-col items-center justify-between p-4 sm:p-8 md:p-12 lg:p-16 xl:p-4">
+                <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
+                    {services.map((service, index) => (
+                        <ServicesCard
+                            key={index}
+                            title={service.title}
+                            description={service.description}
+                        />
+                    ))}
+                </div>
+            </section>
+        </main>
+    );
 }
