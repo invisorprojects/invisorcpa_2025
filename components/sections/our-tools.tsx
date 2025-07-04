1532d60749d2e8bd73d2ab3f26185a5b229dd1ac
govindpvenu <govindpvenu.txt@gmail.com>
2025-07-04 11:47:14 +0530

    fix: changed our tools to grid instead of marquee

--- components/sections/our-tools.tsx
+++ components/sections/our-tools.tsx
@@ -1,17 +1,38 @@
-import { Marquee } from '@/components/magicui/marquee';
 import Image from 'next/image';
 
 export default function OurTools() {
     return (
-        <div className="my-8 flex items-center justify-between">
-            <h4 className="mx-8 text-lg font-semibold text-nowrap">
-                OUR TOOLS
-            </h4>
-            <Marquee pauseOnHover className="[--duration:10s]">
+        <section className="mx-auto flex flex-col items-start justify-between gap-4 p-4 sm:flex-row sm:items-center sm:p-8 md:p-12 lg:p-16 xl:p-24">
+            <div className="max-w-lg px-4 py-12 sm:w-1/2">
+                <h6 className="text-secondary mb-2 text-lg font-semibold tracking-wide uppercase">
+                    Our Tools
+                </h6>
+
+                <h2 className="text-primary sm:text- mb-4 text-3xl leading-snug font-bold">
+                    Professional Accounting
+                    <br />
+                    Software Solutions
+                </h2>
+
+                <p className="text-primary text-base leading-relaxed font-medium sm:text-lg">
+                    We leverage industry-leading accounting software and tools
+                    to deliver accurate, efficient, and comprehensive financial
+                    services for your business.
+                </p>
+            </div>
+            <Tools />
+        </section>
+    );
+}
+
+function Tools() {
+    return (
+        <div className="my-4">
+            <div className="grid grid-cols-2 gap-8 px-8 md:grid-cols-3 lg:grid-cols-3">
                 {[1, 2, 3, 4, 5, 6].map((num) => (
                     <div
                         key={num}
-                        className="relative mr-16 aspect-square h-28 w-auto"
+                        className="relative aspect-square h-28 w-auto"
                     >
                         <Image
                             className="absolute object-contain"
@@ -21,7 +42,7 @@ export default function OurTools() {
                         />
                     </div>
                 ))}
-            </Marquee>
+            </div>
         </div>
     );
 }
