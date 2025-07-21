8f0d139c2ec527e18633bd36d5fbccaa55298c2e
govindpvenu <govindpvenu.txt@gmail.com>
2025-07-21 19:30:35 +0530

    feat: live chat api

--- components/layout/LiveChat.tsx
+++ components/layout/LiveChat.tsx
@@ -5,7 +5,7 @@ import { useEffect } from 'react';
 export default function LiveChat() {
     useEffect(() => {
         const script = document.createElement('script');
-        script.src = 'https://embed.tawk.to/6867a13072d72e190b4f14ac/1ivacravp';
+        script.src = 'https://embed.tawk.to/687e45c13b2af81922773516/1j0mk1036';
         script.async = true;
         script.setAttribute('crossorigin', '*');
         document.body.appendChild(script);
