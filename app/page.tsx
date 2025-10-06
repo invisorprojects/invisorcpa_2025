528329c8cf68280484e88c754d0ca5d972d0c61c
govindpvenu <govindpvenu.txt@gmail.com>
2025-10-06 15:04:59 +0530

    trigger build last one

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,6 @@ import { getStoryblokApi } from '@/lib/storyblok';
 import { NumberTicker } from '@/components/magicui/number-ticker';
 import CountUp from '@/components/react-bits/count-up';
 import PricingSection from '@/components/PricingSection';
-export const revalidate = 1;
 
 export const metadata: Metadata = {
     title: 'Accountant For Small Business - Tax Services London Ontario',
