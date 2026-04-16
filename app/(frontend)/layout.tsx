ba52a91c715d724943e9704eacb872a97ec68b98
govindpvenu <govindpvenu.txt@gmail.com>
2026-04-16 14:49:11 +0530

    fix: change mircrosoft clariy tag id

--- app/(frontend)/layout.tsx
+++ app/(frontend)/layout.tsx
@@ -210,7 +210,7 @@ export default function RootLayout({
                                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                            })(window, document, "clarity", "script", "su57cef7ad");
+                            })(window, document, "clarity", "script", "wcinlb76xa");
                         `}
                     </Script>
 
