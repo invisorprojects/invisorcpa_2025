425bf7df85ffede97f5df1947bc008714031a70b
govindpvenu <govindpvenu.txt@gmail.com>
2026-05-11 17:27:30 +0530

    feat: google tag manager in lp and thank you page

--- app/lp/bookkeeping-ontario/layout.tsx
+++ app/lp/bookkeeping-ontario/layout.tsx
@@ -6,6 +6,7 @@ import { Button } from '@/components/ui/button';
 import { openSans, poppins } from '@/constants/FONTS';
 import { ArrowRight, Leaf, Mail, Phone } from 'lucide-react';
 import ScrollLink from './_components/scroll-link';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
     metadataBase: new URL('https://www.invisorcpa.ca'),
@@ -178,6 +179,20 @@ export default function BookkeepingOntarioLayout({
             lang="en"
             className={`${poppins.variable} ${openSans.variable} scroll-smooth antialiased`}
         >
+                   <head>
+       {/* Google Tag Manager */}
+                <Script id="google-tag-manager" strategy="beforeInteractive">
+                    {`
+                        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                        })(window,document,'script','dataLayer','GTM-P9F755W5');
+                    `}
+                </Script>
+                {/* End Google Tag Manager */}
+
+        </head>
             <body>
                 <BookkeepingOntarioHeader />
                 {children}
--- app/thank-you/layout.tsx
+++ app/thank-you/layout.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import '../(frontend)/globals.css';
 import { openSans, poppins } from '@/constants/FONTS';
 
@@ -11,6 +12,20 @@ export default function ThankYouLayout({
             lang="en"
             className={`${poppins.variable} ${openSans.variable} antialiased`}
         >
+                               <head>
+       {/* Google Tag Manager */}
+                <Script id="google-tag-manager" strategy="beforeInteractive">
+                    {`
+                        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                        })(window,document,'script','dataLayer','GTM-P9F755W5');
+                    `}
+                </Script>
+                {/* End Google Tag Manager */}
+
+        </head>
             <body>{children}</body>
         </html>
     );
