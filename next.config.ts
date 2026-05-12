5b5d99bb130df2826fb76c43b8d78b0c96407db7
govindpvenu <govindpvenu.txt@gmail.com>
2026-05-12 18:03:46 +0530

    chore: fix 3

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import { withPayload } from '@payloadcms/next/withPayload';
 import withPlaiceholder from '@plaiceholder/next';
 import type { NextConfig } from 'next';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const currentDir = dirname(fileURLToPath(import.meta.url));
 
 const nextConfig: NextConfig = {
     /* config options here */
+    outputFileTracingRoot: currentDir,
     images: {
         qualities: [100, 75],
         remotePatterns: [
