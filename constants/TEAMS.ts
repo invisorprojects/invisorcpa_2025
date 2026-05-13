17c04265afd54f652d76ccf6490f7bc93f3b331b
govindpvenu <govindpvenu.txt@gmail.com>
2026-05-13 12:24:35 +0530

    fix: reorder teams

--- constants/TEAMS.ts
+++ constants/TEAMS.ts
@@ -1,20 +1,34 @@
 export const TEAMS = [
     {
+        name: 'Geevar Thambi',
+        qualification: 'MBA, CMA, CSCA, CPB, EA',
+        path: '/assets/team/team-members-3.webp',
+    },
+      {
+        name: 'Bernie Keim',
+        qualification: 'MBA, CGA, CPA',
+        path: '/assets/team/team-members-2.webp',
+    },
+
+       {
+        name: 'Anjali Anil',
+        qualification: 'CMA',
+        path: '/assets/team/team-members-7.webp',
+    },
+
+      {
+        name: 'Ajith Kumar',
+        qualification: 'MBA',
+        path: '/assets/team/team-members-6.webp',
+    },
+    {
         name: 'Roji Thomas',
         qualification:
             'Specializes in finance transformation and ERP migrations, with a strong focus on financial reporting and data integrity. He partners with CFOs and finance teams to deliver seamless system transitions, including data migration, validation, and reporting optimization. Roji combines deep financial process knowledge with a client-focused approach to help organizations modernize their financial operations and drive better decision-making.',
         path: '/assets/team/team-members-1.webp',
     },
-    {
-        name: 'Bernie Keim',
-        qualification: 'MBA, CGA, CPA',
-        path: '/assets/team/team-members-2.webp',
-    },
-    {
-        name: 'Geevar Thambi',
-        qualification: 'MBA, CMA, CSCA, CPB, EA',
-        path: '/assets/team/team-members-3.webp',
-    },
+  
+  
     {
         name: 'Alex Tom Joseph',
         qualification: 'CMA',
@@ -26,16 +40,8 @@ export const TEAMS = [
         path: '/assets/team/team-members-5.webp',
     },
 
-    {
-        name: 'Ajith Kumar',
-        qualification: 'MBA',
-        path: '/assets/team/team-members-6.webp',
-    },
-    {
-        name: 'Anjali Anil',
-        qualification: 'CMA',
-        path: '/assets/team/team-members-7.webp',
-    },
+  
+ 
     {
         name: 'Dayana Silvin',
         qualification: 'CMA',
