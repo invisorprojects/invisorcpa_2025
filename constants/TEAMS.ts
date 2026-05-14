05bdd5362a81bb699daf526b1c26962729384639
govindpvenu <govindpvenu.txt@gmail.com>
2026-05-14 20:50:14 +0530

    feat: update teams

--- constants/TEAMS.ts
+++ constants/TEAMS.ts
@@ -9,24 +9,24 @@ export const TEAMS = [
         qualification: 'MBA, CGA, CPA',
         path: '/assets/team/team-members-2.webp',
     },
-
-       {
-        name: 'Anjali Anil',
-        qualification: 'CMA',
-        path: '/assets/team/team-members-7.webp',
+  {
+        name: 'Andrea Alba Vasquez',
+        qualification: 'PCP',
+        path: '/assets/team/andrea.webp',
     },
-
-      {
-        name: 'Ajith Kumar',
-        qualification: 'MBA',
-        path: '/assets/team/team-members-6.webp',
-    },
-    {
+  
+   {
         name: 'Roji Thomas',
         qualification:
             'Specializes in finance transformation and ERP migrations, with a strong focus on financial reporting and data integrity. He partners with CFOs and finance teams to deliver seamless system transitions, including data migration, validation, and reporting optimization. Roji combines deep financial process knowledge with a client-focused approach to help organizations modernize their financial operations and drive better decision-making.',
         path: '/assets/team/team-members-1.webp',
     },
+      {
+        name: 'Ajith Kumar',
+        qualification: 'MBA',
+        path: '/assets/team/team-members-6.webp',
+    },
+ 
   
   
     {
@@ -39,12 +39,34 @@ export const TEAMS = [
         qualification: 'CMA',
         path: '/assets/team/team-members-5.webp',
     },
-
-  
- 
     {
         name: 'Dayana Silvin',
         qualification: 'CMA',
         path: '/assets/team/team-members-8.webp',
+    },
+         {
+        name: 'Anjali Anil',
+        qualification: 'CMA',
+        path: '/assets/team/team-members-7.webp',
+    },
+    {
+        name: 'Sanoop KS',
+        qualification: '',
+        path: '/assets/team/sanoop.webp',
+    },
+       {
+        name: 'Navab Rajan',
+        qualification: '',
+        path: '/assets/team/navab.webp',
+    },
+       {
+        name: 'Vidya Menon',
+        qualification: '',
+        path: '/assets/team/vidya.webp',
+    },
+       {
+        name: 'Jomis John',
+        qualification: '',
+        path: '/assets/team/jomis.webp',
     },
 ];
