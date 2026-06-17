b6f44ca8f3d8561759bf2f819bbbb9f302d1365f
govindpvenu <govindpvenu.txt@gmail.com>
2026-06-17 23:31:16 +0530

    remove verum-nadan logo

--- components/sections/organizations-section.tsx
+++ components/sections/organizations-section.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 
 export function OrganizationsSection() {
     const logos = Array.from(
-        { length: 67 },
+        { length: 66 },
         (_, i) => `/assets/customer-logos/${i + 1}.webp`
     );
 
