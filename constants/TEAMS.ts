fc0d2dd62b63c6631df15715294f509e0bb68df4
govindpvenu <govindpvenu.txt@gmail.com>
2026-06-17 15:34:00 +0530

    fix: remove risky clients & team members restruction

--- constants/TEAMS.ts
+++ constants/TEAMS.ts
@@ -14,7 +14,6 @@ export const TEAMS = [
         qualification: 'PCP',
         path: '/assets/team/andrea.webp',
     },
-  
    {
         name: 'Roji Thomas',
         qualification:
@@ -26,45 +25,42 @@ export const TEAMS = [
         qualification: 'MBA',
         path: '/assets/team/team-members-6.webp',
     },
- 
-  
-  
-    {
-        name: 'Alex Tom Joseph',
-        qualification: 'CMA',
-        path: '/assets/team/team-members-4.webp',
-    },
+    // {
+    //     name: 'Alex Tom Joseph',
+    //     qualification: 'CMA',
+    //     path: '/assets/team/team-members-4.webp',
+    // },
 
-    {
-        name: 'Dayana Silvin',
-        qualification: 'CMA',
-        path: '/assets/team/team-members-8.webp',
-    },
-        {
-        name: 'Mohammed Shafeeque',
-        qualification: 'CMA',
-        path: '/assets/team/team-members-5.webp',
-    },
-         {
-        name: 'Anjali Anil',
-        qualification: 'CMA',
-        path: '/assets/team/team-members-7.webp',
-    },
-    {
-        name: 'Sanoop KS',
-        qualification: '',
-        path: '/assets/team/sanoop.webp',
-    },
-       {
-        name: 'Navab Rajan',
-        qualification: '',
-        path: '/assets/team/navab.webp',
-    },
-       {
-        name: 'Vidya Menon',
-        qualification: '',
-        path: '/assets/team/vidya.webp',
-    },
+    // {
+    //     name: 'Dayana Silvin',
+    //     qualification: 'CMA',
+    //     path: '/assets/team/team-members-8.webp',
+    // },
+    //     {
+    //     name: 'Mohammed Shafeeque',
+    //     qualification: 'CMA',
+    //     path: '/assets/team/team-members-5.webp',
+    // },
+    //      {
+    //     name: 'Anjali Anil',
+    //     qualification: 'CMA',
+    //     path: '/assets/team/team-members-7.webp',
+    // },
+    // {
+    //     name: 'Sanoop KS',
+    //     qualification: '',
+    //     path: '/assets/team/sanoop.webp',
+    // },
+    //    {
+    //     name: 'Navab Rajan',
+    //     qualification: '',
+    //     path: '/assets/team/navab.webp',
+    // },
+    //    {
+    //     name: 'Vidya Menon',
+    //     qualification: '',
+    //     path: '/assets/team/vidya.webp',
+    // },
        {
         name: 'Jomis John',
         qualification: '',
