0e7025c31e788a3a57b4c238cdb132c38ba5053d
Govind P Venu <govindpvenu.txt@gmail.com>
2026-09-09 21:06:14 +0530

    fix: change initial office to london at review

--- app/review/page.tsx
+++ app/review/page.tsx
@@ -9,5 +9,5 @@ export const metadata: Metadata = {
 };
 
 export default function RequestReviewPage() {
-    return <ReviewAssistant initialOffice="Fergus" />;
+    return <ReviewAssistant initialOffice="London" />;
 }
